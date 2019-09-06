4425e0c8ff837d4ede21c1f2ceb4892f0b491bd4
Eddy Tyannikov <etyannikov@gmail.com>
2019-09-06 15:46:54 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -5,3 +5,25 @@ end
 def using_unshift
   
 end
+
+def using_pop
+  
+end
+
+def using_pop_with_args
+  
+end
+
+def using_shift
+  
+end
+
+def using_shift_with_args
+  
+end
+
+def using_concat
+  
+end
+
+def 
